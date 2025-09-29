ab7315b0e967d400c50952a3f6c1fcd42a2cb976
Manuel Cabrera <josemanuelcabrera2011@gmail.com>
2025-09-28 18:14:23 -0600

    add routes to control de user  registration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', redirectTo: 'user_registration', pathMatch: 'full'
+    },
+    {
+        path: 'user_registration', loadComponent: () => import('./shared/components/user/user.component').then(m => m.UserComponent)
+    },
+    {
+        path: '**', loadComponent: () => import('./shared/components/not-found/not-found.component').then(m => m.NotFoundComponent)
+    }
+];
